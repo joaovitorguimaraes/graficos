6ed9846c958c93c87b6b5f3363583ffa26587938
joao vitor <102987009+joaovitorguimaraes@users.noreply.github.com>
2024-10-17 11:30:25 -0300

    Add files via upload

---
+++ quantidadeusuarios.js
@@ -0,0 +1,31 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+          x: 'nomeDasRedes',
+          y: quantidadeDeUsuarios,
+          type: 'bar',
+          marker: {
+            color: getCSS('--primary-color')
+          }
+        }
+      ]
+
+      const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
+
+    }
+
+      const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data, layout)
+
+  quantidadeUsuarios()
